08dbf2abcb89384d17d4ef65da87a2efc10c17e8
Raphael <raphael.saraiva13@gmail.com>
2022-01-12 15:37:41 +0000

    add times

--- client-go/main.go
+++ client-go/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 	"myapp/certificate"
 	"myapp/connection"
 	"os"
@@ -15,7 +16,7 @@ func main() {
 	var (
 		certName    = CLIENT_NAME + "_cert.pem"
 		privKeyName = CLIENT_NAME + "_privkey.key"
-		hostname    = "0.0.0.0"
+		hostname    = "129.159.50.106"
 		port        = 443
 	)
 	if _, err := os.Stat(certName); os.IsNotExist(err) {
@@ -23,15 +24,28 @@ func main() {
 		certificate.GenerateSelfSignedCert(CLIENT_NAME, 2048, certName, privKeyName)
 	}
 
+	timeBefore := time.Now().UnixNano() / int64(time.Millisecond)
+	fmt.Printf("time before:::  %d\n",timeBefore)
+
 	conn, err := connection.ConnectToServer(hostname, port, certName, privKeyName)
 	if err != nil {
 		log.Fatalf("Erro ao conectar com servidor remoto %v:%v : %v", hostname, port, err)
 	}
 
-	message := "teste"
+	timeAfter := time.Now().UnixNano() / int64(time.Millisecond)
+	fmt.Printf("time after:::  %d\n",timeAfter)
+
+	time :=  timeAfter - timeBefore
+	fmt.Printf("transmission time:::  %d\n",time)
+
+	//envio da mensagem
+	message := "teste GO"
 	n, err := io.WriteString(conn, message)
+
 	if err != nil {
 		log.Fatalf("Erro ao enviar a mensagem. %s", err)
 	}
 	log.Printf("Client enviou %q (%d bytes)", message, n)
+
+
 }
--- client-js/main.js
+++ client-js/main.js
@@ -5,9 +5,8 @@ var fs = require('fs');
 
 
 const PORT = 443;
-const HOST = '0.0.0.0'
+const HOST = '129.159.50.106'
 
-// Pass the certs to the server and let it know to process even unauthorized certs.
 var options = {
     key: fs.readFileSync('./ssl/CLIENT_JS_privkey.key'),
     cert: fs.readFileSync('./ssl/CLIENT_JS_cert.pem'),
@@ -16,43 +15,37 @@ var options = {
     rejectUnauthorized: false
 };
 
+var timeBefore = Date.now();
+console.log("time before::: ", timeBefore);
 var client = tls.connect(PORT, HOST, options, function() {
-
-    // Check if the authorization worked
     if (client.authorized) {
         console.log("Connection authorized by a Certificate Authority.");
     } else {
         console.log("Connection not authorized: " + client.authorizationError)
     }
+    var timeAfter= Date.now();
+    console.log("time after::: ", timeAfter);
+    console.log("transmission time::: ", timeAfter - timeBefore);
 
-    // Send a friendly message
+    //envio da mensagem
     client.write("teste JS");
+    return
 
 });
 
 client.on("data", function(data) {
-
     console.log('Received: %s [it is %d bytes long]',
         data.toString().replace(/(\n)/gm,""),
         data.length);
 
-    // Close the connection after receiving the message
     client.end();
-
 });
 
 client.on('close', function() {
-
     console.log("Connection closed");
-
 });
 
-// When an error ocoures, show it.
 client.on('error', function(error) {
-
     console.error(error);
-
-    // Close the connection after the error occurred.
     client.destroy();
-
 });
